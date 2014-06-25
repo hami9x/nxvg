80625d8983c335d1927b280b8967fc327e8eb5be
phaikawl <phaikawl@gmail.com>
2014-06-25 11:20:09 +0700

    simple #include support for shaders

--- shader.cpp
+++ shader.cpp
@@ -6,6 +6,8 @@
 #include <cstdarg>
 #include <fstream>
 #include <iostream>
+#include <regex>
+#include <sstream>
 
 using namespace std;
 
@@ -37,38 +39,60 @@ void printShaderLog(GLuint shader) {
     }
 }
 
+static inline std::string &ltrim(std::string &s) {
+        s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+        return s;
+}
+
+const std::string IMPORTDEF = "#include";
+const std::string SHADERBASE = "shaders/";
+
+void getShaderSource(std::stringstream & sstream, std::string filepath) {
+    std::ifstream sourceFile(SHADERBASE+filepath);
+    if (sourceFile) {
+        for (std::string line; getline(sourceFile, line);) {
+            if (line.substr(0, IMPORTDEF.size()) == IMPORTDEF) {
+                auto ifile = line.substr(IMPORTDEF.size()+2, line.size()-IMPORTDEF.size()-3);
+                getShaderSource(sstream, SHADERBASE+ifile);
+            } else {
+                sstream << line << std::endl;
+            }
+        }
+    } else {
+        printf("Unable to open file %s\n", filepath.c_str());
+        FAIL("");
+    }
+}
+
 GLuint loadShaderFromFile(std::string path, GLenum shaderType) {
     //Open file
     GLuint shaderID = 0;
     std::string shaderString;
-    std::ifstream sourceFile(path.c_str());
 
-    //Source file loaded
-    if(sourceFile) {
-        //Get shader source
-        shaderString.assign((std::istreambuf_iterator< char >(sourceFile)), std::istreambuf_iterator< char >());
-        //Create shader ID
-        shaderID = glCreateShader(shaderType);
+    std::stringstream source;
 
-        //Set shader source
-        const GLchar* shaderSource = shaderString.c_str();
-        glShaderSource(shaderID, 1, (const GLchar**)&shaderSource, NULL);
+    getShaderSource(source, path);
+    //Get shader source
+    shaderString = source.str();
 
-        //Compile shader source
-        glCompileShader(shaderID);
+    //Create shader ID
+    shaderID = glCreateShader(shaderType);
 
-        //Check shader for errors
-        GLint shaderCompiled = GL_FALSE;
-        glGetShaderiv(shaderID, GL_COMPILE_STATUS, &shaderCompiled);
-        if(shaderCompiled != GL_TRUE) {
-            printf("Unable to compile shader %d!\n\nSource:\n%s\n", shaderID, shaderSource);
-            printShaderLog(shaderID);
-            glDeleteShader(shaderID);
-            shaderID = 0;
-        }
-    }
-    else {
-        printf("Unable to open file %s\n", path.c_str());
+    //Set shader source
+    const GLchar* shaderSource = shaderString.c_str();
+    glShaderSource(shaderID, 1, (const GLchar**)&shaderSource, NULL);
+
+    //Compile shader source
+    glCompileShader(shaderID);
+
+    //Check shader for errors
+    GLint shaderCompiled = GL_FALSE;
+    glGetShaderiv(shaderID, GL_COMPILE_STATUS, &shaderCompiled);
+    if(shaderCompiled != GL_TRUE) {
+        printf("Unable to compile shader %d!\n\nSource:\n%s\n", shaderID, shaderSource);
+        printShaderLog(shaderID);
+        glDeleteShader(shaderID);
+        shaderID = 0;
     }
 
     return shaderID;
@@ -78,14 +102,13 @@ Program::Program(): m_program(0), m_vao(0), m_vbo(0) {
 }
 
 Program::Program(const char *vshader, const char *fshader): m_program(0), m_vao(0), m_vbo(0) {
-    std::string shaderBase("shaders/");
     glGenVertexArrays(1, &m_vao);
 
     // Create a Vertex Buffer Object and copy the vertex data to it
     glGenBuffers(1, &m_vbo);
 
-    GLuint vertexShader = loadShaderFromFile(shaderBase+vshader, GL_VERTEX_SHADER);
-    GLuint fragmentShader = loadShaderFromFile(shaderBase+fshader, GL_FRAGMENT_SHADER);
+    GLuint vertexShader = loadShaderFromFile(vshader, GL_VERTEX_SHADER);
+    GLuint fragmentShader = loadShaderFromFile(fshader, GL_FRAGMENT_SHADER);
 
     m_program = glCreateProgram();
     glAttachShader(m_program, vertexShader);
